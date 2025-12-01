40e4b133bddea5708d9384ad4f75f40cbe6deac1
kelvyn2012 <kudufo@gmail.com>
2025-12-01 14:04:22 +0100

    made use of jason response instaed of render

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
 @cache_page(60 * 15)   # 15 minutes
 def property_list(request):
-    properties = Property.objects.all()
-    return render(request, "properties/property_list.html", {"properties": properties})
+    properties = Property.objects.all().values()  # get all fields
+    data = list(properties)  # convert QuerySet to list of dicts
+    return JsonResponse({"data": data})
+
+
 
